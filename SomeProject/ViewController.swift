d3ffcdb09d3735149e0a54349760702d0292ff17
Nikita Petrenkov <nikita.petrenkov@rtlabs.ru>
2020-11-02 21:36:44 +0400

    Code with issues

--- SomeProject/ViewController.swift
+++ SomeProject/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    override func viewDidLoad( {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+    var sample = "пример"
+    print(sample)
+    
+    if false {
+        print("Будет ли выполнена эта строка?")
+        someMetho()
     }
-
-
+}
+    
+    func someMethod() {
+        
+    }
+    
 }
 
